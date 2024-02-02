8fcbd5c8a05cd8b054b78880bf2a02306872e871
MYXH <1735350920@qq.com>
2024-02-02 17:16:10 +0800

    《ChatGPT 微服务应用体系构建》 - dev-ops 第6节：前后端构建镜像部署

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-docker run -d --name chatgpt-web -p 3000:3000 myxh/chatgpt-web:1.0
+#  启动本地 Docker 镜像
+# docker run -d --name chatgpt-web-local -p 3000:3000 myxh/chatgpt-web-local:1.0
+
+#  启动云服务器 Docker 镜像
+docker run -d --name chatgpt-web-cloud -p 3000:3000 myxh/chatgpt-web-cloud:1.0
