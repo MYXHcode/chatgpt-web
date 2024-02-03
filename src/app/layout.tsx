d14ef184ecc76986f82c2d99019e00180afcd3d6
MYXH <1735350920@qq.com>
2024-02-03 20:20:29 +0800

    《ChatGPT 微服务应用体系构建》 - dev-ops 第7节：网站添加百度统计

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,24 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+        <head>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              var _hmt = _hmt || [];
+              (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?0a531fafc00be8a3305e467f18568989";
+                var s = document.getElementsByTagName("script")[0]; 
+                s.parentNode.insertBefore(hm, s);
+              })();
+            `,
+                }}
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+            <title></title>
+        </head>
+
         <body className={inter.className}>{children}</body>
         </html>
     )
